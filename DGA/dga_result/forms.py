4d1ef6617f3e90b26cd71ba47a11beaacf17cc45
Samsul Alam Shumon <samsul.alam.stp@gmail.com>
2023-01-10 22:04:42 +0600

    duval triangles internal conditions

--- DGA/dga_result/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import DGA_Values
-
-class DGA_form(forms.ModelForm):
-    class Meta:
-        model = DGA_Values
-        fields = '__all__'
--- DGA/dga_result/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', form_view, name='form_view'),
-    path('data/', data_view, name='data_view'),
-]
